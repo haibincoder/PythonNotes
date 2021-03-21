e3b2d22cc7541788fd7e8cbb1110c980b45e5cfc
bin <haibincoder@outlook.com>
2021-03-21 13:28:13 +0800

    add 61

---
+++ LeetCode/61.py
@@ -0,0 +1,68 @@
+"""
+61. 旋转链表
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+示例 2:
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+"""
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if head is None or head.next is None:
+            return head
+
+        root = head
+
+        cur = head
+        temp = None
+        length = 0
+        while cur:
+            temp = cur
+            cur = cur.next
+            length += 1
+        temp.next = root
+
+        index = 0
+        cur = head
+        move = length - k % length
+        while index < move:
+            temp = cur
+            cur = cur.next
+            index += 1
+
+        temp.next = None
+        return cur
+
+
+if __name__ == "__main__":
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    solution = Solution()
+    solution.rotateRight(node1, 2)
+
